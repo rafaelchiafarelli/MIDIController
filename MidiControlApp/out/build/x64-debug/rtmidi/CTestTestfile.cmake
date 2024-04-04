3567a9d08d47b93b0bf636c2317efdcaabed6f6f
Rafael Toschi Chiafarelli <rafael.chiafarelli@gmail.com>
2024-04-04 20:06:39 -0300

    house keeping adding rtmidi and removing temp files

--- MidiControlApp/out/build/x64-debug/rtmidi/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Usuário/source/repos/MIDIController/MidiControlApp/rtmidi
-# Build directory: C:/Users/Usuário/source/repos/MIDIController/MidiControlApp/out/build/x64-debug/rtmidi
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[apinames]=] "C:/Users/Usuário/source/repos/MIDIController/MidiControlApp/out/build/x64-debug/rtmidi/tests/apinames.exe")
-set_tests_properties([=[apinames]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Usuário/source/repos/MIDIController/MidiControlApp/rtmidi/CMakeLists.txt;237;add_test;C:/Users/Usuário/source/repos/MIDIController/MidiControlApp/rtmidi/CMakeLists.txt;0;")
